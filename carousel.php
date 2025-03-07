2cee2bd47a1445a5edcf7d87f41fc0b78d32c5ff
buckatron <47488933+buckatron@users.noreply.github.com>
2025-03-07 23:25:33 +0000

    fixed links

--- carousel.php
+++ carousel.php
@@ -8,11 +8,12 @@
         <title>Cat Carousel</title>
 
         <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
-        <link src="css/style.css" rel="stylesheet"></link>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
-   rel="stylesheet" crossorigin="anonymous"
-   integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3">
+
     </head>
     <body>
         <!-- Responsive navbar-->
--- index.php
+++ index.php
@@ -39,6 +39,6 @@
         <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
         <!-- Core theme JS-->
         <script src="js/scripts.js"></script>
-        <link src="css/style.css" rel="stylesheet"></link>
+        <link rel="stylesheet" href="css/style.css">
     </body>
 </html>
